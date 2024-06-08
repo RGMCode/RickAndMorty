79ae7e53e7d18aa35e55e9fd611e24e2168456b9
RGMCode <coding@mengedoth.net>
2024-06-08 16:00:38 +0200

    add RaMModel

---
+++ RickAndMorty/RaMModel.swift
@@ -0,0 +1,22 @@
+//
+//  RaMModel.swift
+//  RickAndMorty
+//
+//  Created by RGMCode on 08.06.24.
+//
+
+import SwiftUI
+import Foundation
+
+struct Character: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let image: String
+}
+
+struct ChracterResponse: Codable {
+    let results: [Character]
+}
